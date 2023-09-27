b99b1882e69625a8e5a9471591ac60fd0f6a27c5
Danfok69 <140873163+Danfok69@users.noreply.github.com>
2023-09-27 17:02:23 +0500

    Загрузил задание

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def compound_match(words, target):
+    a = [i for i in range(len(words)) if words[i] in target]
+    for i in range(len(a)):
+        if target.replace(words[a[i-1]], "") == words[a[i]]:
+            b = [words[a[i-1]], words[a[i]]]
+    c = [i for i in (b if b[0] + b[1] == target else b[::-1])]
+    c.append([words.index(c[0]), words.index(c[1])])
+    print(a, b, c)
+    return c
